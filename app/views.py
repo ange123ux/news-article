3bdcb1b4d55b8948d74f280a168aeffa5ceefbb2
Ange Uwimana <uuwange@gmail.com>
2020-11-22 04:11:27 +0200

    added new route with new view function

--- app/views.py
+++ app/views.py
@@ -9,3 +9,26 @@ def index():
     View root page function that returns the index page and its data
     '''
     return render_template('index.html')
+
+@app.route('/new/<new_id>')
+def new(new_id):
+
+    '''
+    View new page function that returns the new details page and its data
+    '''
+    return render_template('new.html',id = new_id)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
